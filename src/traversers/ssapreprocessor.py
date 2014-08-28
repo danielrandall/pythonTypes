9c7e7f64ce33f0cf8dbe4cc182c2d09a15565493
danielrandall <daniel.randall10@imperial.ac.uk>
2014-08-28 13:34:31 +0100

    SSA now works better when editing global vars and function arguments.

--- src/traversers/ssapreprocessor.py
+++ src/traversers/ssapreprocessor.py
@@ -14,6 +14,8 @@ class SSA_Pre_Processor(AstFullTraverser):
         self.in_loop_test = False
         self.ssa_exempts = set()
         self.local_variables = None
+        self.global_variables = None
+        self.func_global_stores = None
         
     def run(self, source):
         self.visit(source)
@@ -25,17 +27,19 @@ class SSA_Pre_Processor(AstFullTraverser):
         return method(node)
     
     def do_Module(self, node):
+        self.global_variables = node.global_variables
         for dependent in node.import_dependents:
             self.ssa_exempts.add(dependent.get_as_name())
             
         self.process_blocks(node.initial_block)
         for z in node.body:
             self.visit(z)
-        pass
             
     def do_ClassDef(self, node):
         
         self.ssa_exempts.add(node.name)
+        old_globals = self.global_variables
+        self.global_variables = node.global_variables
         old_ssa_exempts = self.ssa_exempts.copy()
         try:
             for z in node.bases:
@@ -45,6 +49,7 @@ class SSA_Pre_Processor(AstFullTraverser):
             for z in node.decorator_list:
                 self.visit(z)
         finally:
+            self.global_variables = old_globals
             self.ssa_exempts = old_ssa_exempts 
         
     
@@ -53,6 +58,9 @@ class SSA_Pre_Processor(AstFullTraverser):
         old_locals = self.local_variables
         node.local_variables = set()
         self.local_variables = node.local_variables
+        old_func_store = self.func_global_stores
+        node.func_global_stores = set()
+        self.func_global_stores = node.func_global_stores
         
         self.ssa_exempts.add(node.name)
         old_ssa_exempts = self.ssa_exempts.copy()
@@ -65,6 +73,7 @@ class SSA_Pre_Processor(AstFullTraverser):
             print("Local variables for " + node.name)
             print(self.local_variables)
             self.local_variables = old_locals
+            self.func_global_stores = old_func_store
             self.ssa_exempts = old_ssa_exempts 
             
     def do_arguments(self,node):
@@ -137,6 +146,8 @@ class SSA_Pre_Processor(AstFullTraverser):
         
     def do_Name(self, node):
         # Global variable
+        if node.id == "bytes_":
+            pass
         if isinstance(node.ctx, ast.Store) and (isinstance(node.stc_context, ast.Module) or isinstance(node.stc_context, ast.ClassDef)):
             self.ssa_exempts.add(node.id)
         if isinstance(node.stc_context, ast.Module):
@@ -145,13 +156,17 @@ class SSA_Pre_Processor(AstFullTraverser):
             return
         if node.id == "self" or node.id == "cls":
             return
+        # Calculate local variables
+        if isinstance(node.ctx, ast.Store) and isinstance(node.stc_context, ast.FunctionDef) and node.id not in self.ssa_exempts:
+            self.local_variables.add(node.id)
+            
+        if node.id in self.ssa_exempts and isinstance(node.ctx, ast.Store):
+            self.global_phis.add(node.id)      
+            self.func_global_stores.add(node.id)     
+            
         if node.id in BUILTIN_TYPE_DICT:
             return
-        if node.id in self.ssa_exempts:
-            return
-        # Calculate local variables
-        if isinstance(node.ctx, ast.Store) and isinstance(node.stc_context, ast.FunctionDef):
-            self.local_variables.add(node.id)
+        
         self.current_block.referenced_vars.add(node.id)
         
         
@@ -161,6 +176,8 @@ class SSA_Pre_Processor(AstFullTraverser):
         if block.start_line_no == "Exit":
             return
         self.current_block = block
+        block.global_phis = set()
+        self.global_phis = block.global_phis
         pred_nos = [block.start_line_no for block in block.predecessors]
         exit_nos = [block.start_line_no for block in block.exit_blocks]
    #     pprint("Block starting at: " + str(block.start_line_no) + " to " + str(exit_nos))
--- src/traversers/ssatraverser.py
+++ src/traversers/ssatraverser.py
@@ -20,7 +20,8 @@ class SSA_Traverser(AstFullTraverser):
         self.global_var_edits = None
         self.local_variables = None
         self.function_ssa_tracker = {}
-        self.block_tracker = {}
+        self.func_global_stores = None
+       # self.block_tracker = {}
         self.in_function = False
             # Keys are names, values are lists of a.values
             # where a is an assignment. 
@@ -53,12 +54,12 @@ class SSA_Traverser(AstFullTraverser):
         if block.start_line_no == 21:
             pass
         
-        self.block_tracker = block.d
+        self.current_block = block
         # Update all but those created by phis
-        self.block_tracker.update({k:v for k,v in self.function_ssa_tracker.items() if k not in self.block_tracker})
+        self.current_block.tracker.update({k:v for k,v in self.function_ssa_tracker.items() if k not in self.current_block.tracker and k not in self.func_global_stores})
         for statement in block.statements:
             self.visit(statement)
-        dict_to_pass = self.block_tracker.copy()
+        dict_to_pass = self.current_block.tracker.copy()
         
      #   print("Block " + str(block.start_line_no) + " to")
      #   print(block.exit_blocks)        
@@ -88,47 +89,56 @@ class SSA_Traverser(AstFullTraverser):
             return
         if block.has_phi_nodes:
             return
-        self.add_phi_nodes(block, local_variables)
+        global_phis = self.add_phi_nodes(block, local_variables)
         for an_exit in block.exit_blocks:
-            self.add_all_phi_nodes(an_exit, local_variables)
+            self.add_all_phi_nodes(an_exit, local_variables | global_phis)
             
     def add_phi_nodes(self, block, local_variables):
         ''' Only add phi nodes with blocks with more than entry point. '''
         block.phi_nodes = {}
-        block.d = {}
+        block.tracker = {}
         block.statements[0].phi_nodes = []
-        for ref_var in local_variables:
+        global_phis = block.global_phis - self.global_var_edits
+        for ref_var in local_variables - global_phis:
             new_phi = self.create_new_phi_node(ref_var, block)
-            new_phi.lineno = block.statements[0].lineno
             block.phi_nodes[ref_var] = new_phi
             block.statements[0].phi_nodes.append(new_phi)
-    #        print("Phi created for block " + str(block.start_line_no))
-    #        print(block.phi_nodes)
+            print("Phi created for block " + str(block.start_line_no))
+            print(block.phi_nodes)
+        for ref_var in global_phis:
+            new_phi = self.create_new_phi_node(ref_var, block)
+            block.phi_nodes[ref_var] = new_phi
+            block.statements[0].phi_nodes.append(new_phi)
+            # Need to add un-labelled global
+            new_phi.update_targets(ref_var)
+            print("Phi created for block " + str(block.start_line_no))
+            print(block.phi_nodes)
         block.has_phi_nodes = True
+        return global_phis
             
     def update_phis(self, block, predecessor_dict):
         if block.start_line_no == "Exit":
             return
         for var, num in predecessor_dict.items():
-            if var not in self.global_variables:
                     # If it doesn't exist yet - can happen when transforming global to local -
                     # add it
                     if var not in block.phi_nodes:
                         new_phi = self.create_new_phi_node(var, block)
                         block.phi_nodes[var] = new_phi
+
                     # Don't add the var if it is itself!
                     if var + str(num) == block.phi_nodes[var].get_var():
                         continue
                     block.phi_nodes[var].update_targets(var + str(num))
-     #               print("Phis for: " + str(block.start_line_no))
-     #               print(block.phi_nodes)
+                    print("Phis for: " + str(block.start_line_no))
+                    print(block.phi_nodes)
                     
     def create_new_phi_node(self, var, block):
         if var in self.function_ssa_tracker:  # Needed for before when var may not yet exist
             self.function_ssa_tracker[var] += 1
         else:
             self.function_ssa_tracker[var] = 1
-        block.d[var] = self.function_ssa_tracker[var]
+        block.tracker[var] = self.function_ssa_tracker[var]
         new_phi = Phi_Node(var + str(self.function_ssa_tracker[var]), set())
         new_phi.lineno = block.statements[0].lineno
         return new_phi
@@ -140,6 +150,8 @@ class SSA_Traverser(AstFullTraverser):
         
     def do_Name(self, node):
         # We don't SSA a global variable
+        if node.id == "tag":
+            pass
         if isinstance(node.stc_context, ast.Module) or isinstance(node.stc_context, ast.ClassDef):
             return
         if node.id == "self" or node.id == "cls":
@@ -149,6 +161,8 @@ class SSA_Traverser(AstFullTraverser):
             return
         # In no way will we rename a node with combination
         if node.id in self.global_variables and node.id in self.global_var_edits:
+            return
+        if node.id in self.func_global_stores and node.id not in self.current_block.phi_nodes:
             return
         # Becomes local variable in this context
         if node.id in self.global_variables and node.id not in self.global_var_edits and isinstance(node.ctx, ast.Store):
@@ -161,7 +175,7 @@ class SSA_Traverser(AstFullTraverser):
                 self.function_ssa_tracker[node.originalId] += 1
             else:
                 self.function_ssa_tracker[node.originalId] = 1
-            self.block_tracker[node.originalId] = self.function_ssa_tracker[node.originalId]
+            self.current_block.tracker[node.originalId] = self.function_ssa_tracker[node.originalId]
         # If it's x.y attribute and a ast.Load then for now we assume that it's already present
         ''' TODO: Check all possible attributes. '''
         if isinstance(node.ctx, ast.Load) and isinstance(node, ast.Attribute):
@@ -169,7 +183,7 @@ class SSA_Traverser(AstFullTraverser):
                 
        # pprint(self.function_ssa_tracker)
         try:
-            node.id = node.originalId + str(self.block_tracker[node.originalId])
+            node.id = node.originalId + str(self.current_block.tracker[node.originalId])
         except:
             pass
 
@@ -195,7 +209,12 @@ class SSA_Traverser(AstFullTraverser):
         self.global_var_edits = node.global_var_edits
         old_tracker = self.function_ssa_tracker.copy()
         old_in_function = self.in_function
+        old_func_stores = self.func_global_stores
+        self.func_global_stores = node.func_global_stores
         
+        
+        if node.name == "__init__":
+            pass
     #    args = self.visit(node.args)
     #    self.fun_args += args
        # print(node.name)
@@ -206,6 +225,7 @@ class SSA_Traverser(AstFullTraverser):
             self.local_variables = node.local_variables
             self.add_all_phi_nodes(node.initial_block, node.local_variables)
             self.blocks_to_process.append((node.initial_block, None))
+            self.old_func_stores = old_func_stores
             self.process_block_list()
         finally:
             self.in_function = old_in_function
