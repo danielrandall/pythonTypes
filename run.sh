915b9afe9fc569ec68369fb779fe70d0b5f72975
danielrandall <daniel.randall10@imperial.ac.uk>
2014-09-15 11:21:10 +0100

    Program now runs from the command line nicely.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PYTHONPATH="${PYTHONPATH}:/homes/dr1810/4thYear/individualProject/pythonTypes"
+export PYTHONPATH
+python3 src/main.py $1
--- src/main.py
+++ src/main.py
@@ -2,15 +2,37 @@ import src.extractpysfromdirectory as file_extractor
 from src.typechecking.typeinferrer import TypeInferrer
 from src.typechecking.errorissuer import ErrorIssuer
 from src.stats import Statistics
-import timeit
+from src.typechecking.basictypevariable import BasicTypeVariable
+from src.typechecking.argtypevariable import ArgTypeVariable
+from src.typeclasses import Int_Type, Float_Type, String_Type
 
+import timeit
+import sys
 
 # Open source Python 3 projects to test on: https://pypi.python.org/pypi?%3Aaction=browse&c=533&show=all
 
 
 if __name__ == '__main__':
+    
+  #  arg_type = ArgTypeVariable()
+  #  arg_type.arg_uses.add(BasicTypeVariable([Int_Type(), Float_Type(), String_Type()]))
+  #  arg_type.arg_uses.add(BasicTypeVariable([Int_Type(), String_Type()]))
+  #  extracted = arg_type.extract_types()
+  #  arg_type.is_same_as_self(BasicTypeVariable(list(extracted)))
+  #  arg_type.types |= extracted
+  #  arg_type.arg_uses.add(BasicTypeVariable([Int_Type()]))
 
-    top_level = "/homes/dr1810/4thYear/individualProject/testFiles/realFiles/Glances-2.0.1"
+   # extracted = arg_type.extract_types()
+   # arg_type.is_same_as_self(BasicTypeVariable(list(extracted)))
+   # assert(False)
+   
+    print(sys.argv)
+    assert len(sys.argv) == 2, "Enter only one directory."
+    top_level = sys.argv[1]
+
+    
+    #top_level = "/homes/dr1810/4thYear/individualProject/demos/demo4"
+ #   top_level = "/homes/dr1810/4thYear/individualProject/testFiles/realFiles/a"
     
     start = timeit.default_timer()
     
